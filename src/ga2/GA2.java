78329bd2d5e1d7a3a11d484bb1b07b4f017609c4
Michael Levin <levin.michael.w@gmail.com>
2021-08-18 19:08:22 -0500

    testing git

--- src/ga2/GA2.java
+++ src/ga2/GA2.java
@@ -33,6 +33,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+// testing
+
 public class GA2  {
 
     public static void main(String[] args) throws Exception 
