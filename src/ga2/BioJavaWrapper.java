2a6250798a0037d3d5cf384b733a4eb288a80c4a
Michael Levin <levin.michael.w@gmail.com>
2021-08-31 22:00:17 -0500

    switch to global alignment

--- src/ga2/BioJavaWrapper.java
+++ src/ga2/BioJavaWrapper.java
@@ -71,7 +71,7 @@ public class BioJavaWrapper
         {
             ProteinSequence given = new ProteinSequence(aaseq);
             
-            return active.getAlignment(given, targetProtein) * aaseq.length() / CallBlast.target.length();
+            return active.getAlignment(given, targetProtein);
         }
         catch(CompoundNotFoundException ex)
         {
@@ -89,7 +89,7 @@ public class BioJavaWrapper
     public double getAlignment(ProteinSequence s1, ProteinSequence s2)
     {
         PairwiseSequenceAligner<ProteinSequence, AminoAcidCompound> smithWaterman =
-                        Alignments.getPairwiseAligner(s1, s2, Alignments.PairwiseSequenceAlignerType.LOCAL, penalty, matrix);
+                        Alignments.getPairwiseAligner(s1, s2, Alignments.PairwiseSequenceAlignerType.GLOBAL, penalty, matrix);
 
         SequencePair<ProteinSequence, AminoAcidCompound> pair = smithWaterman.getPair();
         return pair.getPercentageOfIdentity(true);
